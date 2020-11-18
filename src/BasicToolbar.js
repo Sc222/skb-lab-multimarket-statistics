3edbf7ef4b8b951b59b2f022b3180a1e361a9a7f
Sc222 <v.a.shaternikov@gmail.com>
2020-11-19 04:04:46 +0500

    install Material UI dependencies

---
+++ src/BasicToolbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+export default function Toolbar(){
+    return (<AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+                News
+            </Typography>
+            <Button color="inherit">Login</Button>
+        </Toolbar>
+    </AppBar>);
+}
