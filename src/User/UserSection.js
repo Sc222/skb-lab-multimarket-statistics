e0eff99c1a46210ed19ec957b24b4c4efbd5994d
Sc222 <v.a.shaternikov@gmail.com>
2020-11-26 03:09:09 +0500

    add notifications and profile popovers

--- src/User/UserSection.js
+++ src/User/UserSection.js
@@ -10,7 +10,7 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
 import Avatar from "@material-ui/core/Avatar";
 
-import {Route, Switch as RouteSwitch, useLocation, useParams} from 'react-router-dom';
+import {Link as RouterLink, Route, Switch as RouteSwitch, useLocation, useParams} from 'react-router-dom';
 
 //image imports
 import demoProfile from '../images/demo_profile.png';
@@ -19,6 +19,13 @@ import {HomepageUrl} from "../App";
 import Applications from "./Applications";
 import Application from "./Application";
 import NewApplication from "./NewApplication";
+import Popover from "@material-ui/core/Popover";
+import Divider from "@material-ui/core/Divider";
+import {NewReleasesRounded} from "@material-ui/icons";
+import Box from "@material-ui/core/Box";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
 
 const drawerWidth = 260;
 
@@ -30,6 +37,11 @@ const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
     },
+
+    flex: {
+        display: 'flex'
+    },
+
     toolbarIcon: {
         display: 'flex',
         alignItems: 'center',
@@ -155,6 +167,22 @@ const useStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(1),
     },
 
+    //popover styles
+    popover: {
+        width: theme.spacing(30),
+        maxHeight: theme.spacing(25)
+    },
+
+    fullWidth: {
+        width: '100%'
+    },
+    popoverContainer: {
+        paddingRight: theme.spacing(1.5),
+        paddingLeft: theme.spacing(1.5),
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    }
+
 }));
 
 export default function UserSection() {
@@ -164,6 +192,9 @@ export default function UserSection() {
 
     const regexpUrlWithoutDrawer = new RegExp(`^${HomepageUrl}\/user\/[a-zA-Z0-9-]+\/(apps|new-app)`);
     const regexpUrlWithDrawer = new RegExp(`^${HomepageUrl}\/user\/[a-zA-Z0-9-]+\/app\/[a-zA-Z0-9-]+\/(dashboard|settings)`);
+
+    const [notificationPopoverAnchor, setNotificationPopoverAnchor] = React.useState(null);
+    const [profilePopoverAnchor, setProfilePopoverAnchor] = React.useState(null);
 
     const [isDrawerOpen, setDrawerOpen] = React.useState(true);
 
@@ -175,12 +206,51 @@ export default function UserSection() {
         setDrawerOpen(!isDrawerOpen);
     };
 
+    function logout() {
+        setProfilePopoverAnchor(null);
+        //todo logout
+    }
+
+    function toggleNotificationsPopover(event) {
+        if (notificationPopoverAnchor)
+            setNotificationPopoverAnchor(null);
+        else
+            setNotificationPopoverAnchor(event.currentTarget);
+    }
+
+    function toggleProfilePopover(event) {
+        if (profilePopoverAnchor)
+            setProfilePopoverAnchor(null);
+        else
+            setProfilePopoverAnchor(event.currentTarget.parentElement);
+    }
+
     useEffect(() => {
         // todo load info by username
         console.log(username);
 
         // todo load notifications from server
-        setNotifications(['1', '2']);
+        const notifications = [
+            {
+                title: 'Уведомление 1',
+                description: "Достаточно длинный текст первого уведомления",
+                date: "21 Ноября 2020",
+                new: true
+            },
+            {
+                title: 'Уведомление 2',
+                description: "Достаточно длинный текст второго уведомления",
+                date: "15 Ноября 2020",
+                new: false
+            },
+            {
+                title: 'Уведомление 3',
+                description: "Достаточно длинный текст третьего уведомления",
+                date: "7 Ноября 2020",
+                new: false
+            }
+        ];
+        setNotifications(notifications);
 
         // todo application route params
     }, []);
@@ -217,20 +287,114 @@ export default function UserSection() {
                     </Typography>
 
                     {/*todo show notifications menu on click*/}
-                    <IconButton color="inherit">
+                    <IconButton color="inherit" onClick={(event) => toggleNotificationsPopover(event)}
+                                aria-describedby='notification-popover'>
                         <Badge badgeContent={notifications?.length} color="secondary">
                             <NotificationsIcon/>
                         </Badge>
                     </IconButton>
 
                     {/*todo load profile picture from server*/}
-                    <IconButton className={classes.profileIconButton} color='inherit'>
+                    <IconButton className={classes.profileIconButton} color='inherit'
+                                onClick={(event) => toggleProfilePopover(event)}
+                                aria-describedby='profile-popover'
+                    >
                         <Avatar className={classes.profileIcon} alt='Profile picture' src={demoProfile}/>
                     </IconButton>
                 </Toolbar>
             </AppBar>
-            <main className={classes.content}>
 
+            <Popover
+                id='notification-popover'
+                open={notificationPopoverAnchor !== null}
+                anchorEl={notificationPopoverAnchor}
+                onClose={() => toggleNotificationsPopover(null)}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <div className={classes.popover}>
+                    {/* todo add button to mark notifications as viewed \ delete it */}
+                    {notifications?.map((notification, index) => {
+                        return (
+                            <div>
+                                <div className={classes.popoverContainer}>
+                                    <div className={classes.flex}>
+                                        <Typography variant='body1' color={notification.new ? 'primary' : 'inherit'}>
+                                            {notification.title}
+                                        </Typography>
+                                        {notification.new && <NewReleasesRounded color='primary'/>}
+                                    </div>
+                                    <Typography variant='body2'>{notification.description}</Typography>
+                                    <Typography variant='caption'>{notification.date}</Typography>
+                                </div>
+                                {index !== (notifications.length - 1) && <Divider className={classes.fullWidth}/>}
+                            </div>
+                        );
+                    })
+                    }
+                </div>
+            </Popover>
+
+            <Popover
+                id='profile-popover'
+                open={profilePopoverAnchor !== null}
+                anchorEl={profilePopoverAnchor}
+                onClose={() => toggleProfilePopover(null)}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <div className={classes.popover}>
+                    <div className={classes.popoverContainer}>
+                        <Typography variant='subtitle1' color='primary'>
+                            <Box ml={0.5} fontWeight="fontWeightMedium">
+                                Sc222 </Box>
+                        </Typography>
+                    </div>
+                    <Divider className={classes.fullWidth}/>
+                    <List dense>
+                        {/* todo use repeating technique */}
+                        <ListItem classes={{selected: classes.mainItemSelected}}
+                                  className={classes.mainItem}
+                                  onClick={() => setProfilePopoverAnchor(null)}
+                                  button
+                                  component={RouterLink}
+                                  to={`${HomepageUrl}/user/${username}/apps`}
+                        >
+                            <ListItemText primary={'Приложения'}/>
+                        </ListItem>
+                        <ListItem classes={{selected: classes.mainItemSelected}}
+                                  className={classes.mainItem}
+                                  onClick={() => setProfilePopoverAnchor(null)}
+                                  button
+                                  component={RouterLink}
+                                  to={`${HomepageUrl}/user/${username}/profile`}
+                        >
+                            <ListItemText primary={'Настройки'}/>
+                        </ListItem>
+                        <ListItem classes={{selected: classes.mainItemSelected}}
+                                  className={classes.mainItem}
+                                  onClick={() => logout()}
+                                  button
+                        >
+                            <ListItemText primary={'Выйти'}/>
+                        </ListItem>
+                    </List>
+                </div>
+            </Popover>
+
+            <main className={classes.content}>
                 <RouteSwitch>
                     <Route path={`${HomepageUrl}/user/:username/apps`}>
                         <Applications username={username}/>
