775371727f3c581a291d28f4253fc90ad4f600df
malyshev.v <malyshev.v@skbkontur.com>
2021-01-24 19:14:40 +0500

    fix

--- MultimarketStatistics/MultimarketStatistics/Models/UserUpdateContract.cs
+++ MultimarketStatistics/MultimarketStatistics/Models/UserUpdateContract.cs
@@ -7,5 +7,9 @@ namespace MultimarketStatistics.Models
     public class UserUpdateContract : UserContract
     {
         public string CurrentPassword { get; set; }
+
+        public string NewEmail { get; set; }
+
+        public string NewUsername { get; set; }
     }
 }
