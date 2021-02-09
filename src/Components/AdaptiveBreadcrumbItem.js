7929a663dc0002213b11d9fa651e71037efc7701
Sc222 <v.a.shaternikov@gmail.com>
2021-02-09 15:25:07 +0500

    reformat breadcrumb item

--- src/Components/AdaptiveBreadcrumbItem.js
+++ src/Components/AdaptiveBreadcrumbItem.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-
-
-//image imports
 import {Link as RouterLink} from "react-router-dom";
 import Link from "@material-ui/core/Link";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
-
     textWithIcon: {
         display: 'flex',
         alignItems: 'center',
@@ -18,30 +14,33 @@ const useStyles = makeStyles((theme) => ({
     mR: {
         marginRight: theme.spacing(0.5)
     },
-    iconButtonShrinked: {
+    iconButtonMarginXShrink: {
         marginLeft: theme.spacing(-1.5),
         marginRight: theme.spacing(-1.5)
     }
 }));
 
+//todo PROPS DOCUMENTATION
+//props: text, link, icon, maxLength
 export default function AdaptiveBreadcrumbItem(props) {
     const classes = useStyles();
-
     const Icon = props.icon;
 
     function shortenText(text) {
-        if (text.length <= props.maxLength)
-            return text;
-        return text.slice(0, props.maxLength) + "…";
+        return text.length <= props.maxLength
+            ? text
+            : text.slice(0, props.maxLength) + "…";
     }
 
-    return (<>
+    return (
+        <>
             <Hidden mdUp>
                 {props.maxLength &&
-                <Link color={props.isSelected ? "primary" : "inherit"}
-                      component={RouterLink}
-                      to={props.link}
-                      className={classes.textWithIcon}>
+                <Link
+                    color={props.isSelected ? "primary" : "inherit"}
+                    component={RouterLink}
+                    to={props.link}
+                    className={classes.textWithIcon}>
                     <Icon className={classes.mR}/>
                     {shortenText(props.text)}
                 </Link>
@@ -49,7 +48,7 @@ export default function AdaptiveBreadcrumbItem(props) {
                 {!props.maxLength &&
                 <IconButton
                     color={props.isSelected ? "primary" : "inherit"}
-                    className={classes.iconButtonShrinked}
+                    className={classes.iconButtonMarginXShrink}
                     aria-label={props.text}
                     component={RouterLink}
                     to={props.link}
@@ -57,13 +56,13 @@ export default function AdaptiveBreadcrumbItem(props) {
                     <Icon fontSize='inherit'/>
                 </IconButton>
                 }
-
             </Hidden>
             <Hidden smDown>
-                <Link color={props.isSelected ? "primary" : "inherit"}
-                      component={RouterLink}
-                      to={props.link}
-                      className={classes.textWithIcon}>
+                <Link
+                    color={props.isSelected ? "primary" : "inherit"}
+                    component={RouterLink}
+                    to={props.link}
+                    className={classes.textWithIcon}>
                     <Icon className={classes.mR}/>
                     {props.text}
                 </Link>
