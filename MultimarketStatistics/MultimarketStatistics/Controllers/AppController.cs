f100971bba74add373d69d100f002d54bd3c7552
malyshev.v <malyshev.v@skbkontur.com>
2021-03-05 17:23:46 +0500

    versions sort

--- MultimarketStatistics/MultimarketStatistics/Controllers/AppController.cs
+++ MultimarketStatistics/MultimarketStatistics/Controllers/AppController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -9,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultimarketStatistics.Models;
 using Storage.Entities;
+using Version = Storage.Entities.Version;
 
 namespace MultimarketStatistics.Controllers
 {
@@ -88,15 +88,64 @@ namespace MultimarketStatistics.Controllers
             if (!IsValidAction(userId, appId))
                 return StatusCode(StatusCodes.Status403Forbidden);
 
-            var versions = appService.GetAppVersions(appId);
+            var versions = appService.GetAppVersions(appId).ToList();
+
+            versions.Sort(CompareVersions);
+
             return versions.Where(v => v.Market == market.ToMarketType())
                 .Select(v => v.Number)
+                .Reverse()
                 .ToArray();
         }
 
         private bool IsValidAction(Guid userId, Guid appId)
         {
             return UserIdValidator.IsValidAction(HttpContext, userId) && appService.IsOwnedByUser(userId, appId);
+        }
+
+        private int CompareVersions(Version first, Version second)
+        {
+            var v1 = first.Number?.Split('.');
+            var v2 = second.Number?.Split('.');
+
+            if (v1 == null || v1.Length == 0)
+            {
+                if (v2 == null || v2.Length == 0)
+                    return v1 != null && v2 != null ? first.Number.CompareTo(second.Number) : 0;
+                return 1;
+            }
+
+            if (v2 == null || v2.Length == 0)
+                return -1;
+
+            for (var part = 0; part < v1.Length; ++part)
+            {
+                if (v2.Length == part)
+                    return 1;
+
+                var compareResult = CompareParts(v1[part], v2[part]);
+
+                if (compareResult != 0 || v1.Length == v2.Length && part == v1.Length - 1)
+                    return compareResult;
+            }
+
+            return -1;
+        }
+
+        private int CompareParts(string first, string second)
+        {
+            var isSecondParsable = int.TryParse(second, out var p2);
+            if (int.TryParse(first, out var p1))
+            {
+                if (isSecondParsable)
+                    return p1.CompareTo(p2);
+                return -1;
+            }
+
+            if (isSecondParsable)
+                return 1;
+
+            return first.CompareTo(second);
         }
     }
 }
